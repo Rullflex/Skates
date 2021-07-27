6f97ab6d94b5ee0622201dc17d725cce94ec4f7e
Dmitry <libafer99@gmail.com>
2021-07-27 21:11:52 +0500

    frame

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,22 +1,20 @@
 import UIkit from 'uikit';
 import { app, form } from '../../modules/scripts/_core';
 
-UIkit.util.ready(function () {
-    app.init();
+const { ready, $, $$, play, pause, mute } = UIkit.util;
 
+ready(function () {
+    app.init();
     app.dynamicVideo();
-    // FORM
     form.init(`form`);
 
-    if (document.querySelector(`.form-lead`)) {
-        document.querySelectorAll(`.form-lead__options`).forEach((el) => {
+    if ($(`.form-lead`)) {
+        $$(`.form-lead__options`).forEach((el) => {
             app.letListClickActive(el, 'active', (item) => {
-                el.querySelector('.form-lead__options-input').value = item.dataset.value;
+                $('.form-lead__options-input', el).value = item.dataset.value;
             });
         });
     }
-
-    let switcherDrop;
 
     const sliders = {
         s6: undefined,
@@ -28,16 +26,10 @@ UIkit.util.ready(function () {
     app.matchMediaListener(
         app.md,
         () => {
-            switcherDrop = UIkit.drop('.s3__tab-drop', {
-                mode: 'click',
-            });
-
             sliders.s6 = UIkit.slider('.s6__slider');
             sliders.s13 = UIkit.slider('.s13__slider');
         },
         () => {
-            switcherDrop?.$destroy();
-
             sliders.s6?.$destroy();
             sliders.s13?.$destroy();
         },
@@ -55,12 +47,13 @@ UIkit.util.ready(function () {
         },
     );
 
-    app.sliderSpy('.s6__slider', (idx) => refreshSliderArrowNav('.s6__slider', idx));
-    app.sliderSpy('.s8__slider', (idx) => refreshSliderArrowNav('.s8__slider', idx));
-    app.sliderSpy('.s9__slider', (idx) => refreshSliderArrowNav('.s9__slider', idx));
-    app.sliderSpy('.s13__slider', (idx) => refreshSliderArrowNav('.s13__slider', idx));
-    app.sliderSpy('.s18__slider', (idx) => refreshSliderArrowNav('.s18__slider', idx));
-    app.sliderSpy('.s22__slider', (idx) => refreshSliderArrowNav('.s22__slider', idx));
+    addSliderArrowNav(6, 8, 9, 13, 18, 22);
+
+    function addSliderArrowNav(...slidersIds) {
+        slidersIds.forEach((sliderId) => {
+            app.sliderSpy(`.s${sliderId}__slider`, (idx) => refreshSliderArrowNav(`.s${sliderId}__slider`, idx));
+        });
+    }
 
     function refreshSliderArrowNav(slider, index) {
         document.querySelector(`${slider} .slider-arrow-nav__numbers--current`).innerText = `0${index + 1}`;
@@ -71,7 +64,8 @@ UIkit.util.ready(function () {
         cls: `switcher-active`,
         animation: 'uk-animation-fade',
     });
-    document.querySelectorAll(`li.tab-switcher__content-item`).forEach((el, idx) => {
+
+    $$(`li.tab-switcher__content-item`).forEach((el, idx) => {
         el.addEventListener(`beforeshow`, (event) => {
             if (window.innerWidth < app.md) {
                 const $tabSwitcher = el.closest('.tab-switcher');
@@ -85,14 +79,7 @@ UIkit.util.ready(function () {
 
     app.mapSpy(`.s11__location-map`);
 
-    app.letListClickActive(document.querySelector('.s11__location-nav'), 'active', (el) => {
-        document.querySelector(`.s11__location-map iframe`).setAttribute('src', el.dataset.src);
+    app.letListClickActive($('.s11__location-nav'), 'active', (el) => {
+        $(`.s11__location-map iframe`).setAttribute('src', el.dataset.src);
     });
-
-    // app.letListClickActive(document.querySelector(`ul.list`))
-    // app.dynamicVideo()
-    // app.videoSpy(`#video .popup__body`, 'fmT2FFVuWDA')
-    // app.mapSpy(`#map`, 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2478.681137224115!2d45.91315441602808!3d51.592407112304684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4114c599cd81712b%3A0x2ebcd457d28444b3!2z0YPQuy4g0J_QsNC90YTQuNC70L7QstCwLCA1Mywg0KHQsNGA0LDRgtC-0LIsINCh0LDRgNCw0YLQvtCy0YHQutCw0Y8sIDQxMDAzMw!5e0!3m2!1sru!2sru!4v1618227745566!5m2!1sru!2sru')
-    // const quiz = new Quiz()
-    // quiz.create()
 });
